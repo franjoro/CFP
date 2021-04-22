a7e4e608698d4787aad46e87e22ebf66554b1041
Franklin Lopez <franklin_lopez@ricaldone.edu.sv>
2021-04-22 09:53:16 -0600

    error en correos y mayúscula

--- public/js/habil/formhabil.js
+++ public/js/habil/formhabil.js
@@ -1,43 +1,62 @@
-const ProgressChange = (texto, id, val)=>{
-    $('#progBar').val(val);
-    $("#texto").text(texto);
-    $("#id").text(id);
-}
-
+const ProgressChange = (texto, id, val) => {
+  $("#progBar").val(val);
+  $("#texto").text(texto);
+  $("#id").text(id);
+};
 $(document).ready(function () {
-   // Cambiador de progress
+  // Cambiador de progress
   // BOTONES
   $("#next1").click(() => {
-    ProgressChange("Educación formal y formación ocupacional","2","33.33")
+    ProgressChange("Educación formal y formación ocupacional", "2", "33.33");
     $("#sec1").css("display", "none");
     $("#sec2").css("display", "block");
   });
   $("#next2").click(() => {
-    ProgressChange("Información sobre situación laboral","3","50")
+    ProgressChange("Información sobre situación laboral", "3", "50");
     $("#sec2").css("display", "none");
     $("#sec3").css("display", "block");
   });
   $("#next3").click(() => {
-    ProgressChange("Información sobre ingresos","4","66.33")
+    ProgressChange("Información sobre ingresos", "4", "66.33");
     $("#sec3").css("display", "none");
     $("#sec4").css("display", "block");
   });
   $("#next4").click(() => {
-    ProgressChange("Expectativas y pertenencia de la información","5","82.66")
+    ProgressChange(
+      "Expectativas y pertenencia de la información",
+      "5",
+      "82.66"
+    );
     $("#sec4").css("display", "none");
     $("#sec5").css("display", "block");
   });
   $("#next5").click(() => {
-    ProgressChange("Seguimiento","6","100")
+    ProgressChange("Seguimiento", "6", "100");
     $("#sec5").css("display", "none");
     $("#sec6").css("display", "block");
   });
-
-  // SECCION 1 =========================================================================
   //Mascara
   $("#dui").mask("00000000-0");
   $("#nit").mask("0000-000000-000-0");
-  //Discapacidad
+  $("#year1").mask("0000");
+  $("#year2").mask("0000");
+  $("#year3").mask("0000");
+  $("#telfijocontacto").mask("0000-0000");
+  $("#telmovilcontacto").mask("0000-0000");
+  $("#fijo").mask("0000-0000");
+  $("#movil").mask("0000-0000");
+  // FIN Mascara
+  // fecha
+  $.datepicker.setDefaults( $.datepicker.regional[ "es" ] );
+  $("#fechanac").datepicker({
+    dateFormat: "dd-mm-yy",
+    changeMonth: true,
+    changeYear: true,
+    yearRange: "-100:+0"
+  });
+  // Discapacidad
+  // Fin fecha
+  // SECCION 1 =========================================================================
   $("#discapacidad").click(function () {
     const checkBox = document.getElementById("discapacidad");
     if (checkBox.checked == true) {
@@ -46,15 +65,63 @@ $(document).ready(function () {
       $("#discapacidades").css("display", "none");
     }
   });
+  $("#discapacidades1").click(function () {
+    const checkBox = document.getElementById("discapacidades1");
+    if (checkBox.checked == true) {
+      $("#discapacidades").css("display", "block");
+    }
+  });
+  $("#discapacidades2").click(function () {
+    const checkBox = document.getElementById("discapacidades2");
+    if (checkBox.checked == true) {
+      $("#discapacidades").css("display", "none");
+    }
+  });
   //profesion
-  $("#gridCheck").click(function () {
-    const checkBox = document.getElementById("gridCheck");
+  $("#flexRadioDefault1").click(function () {
+    const checkBox = document.getElementById("flexRadioDefault1");
     if (checkBox.checked == true) {
       $("#profesiondiv").css("display", "block");
-    } else {
+    }
+  });
+  $("#flexRadioDefault2").click(function () {
+    const checkBox = document.getElementById("flexRadioDefault2");
+    if (checkBox.checked == true) {
       $("#profesiondiv").css("display", "none");
     }
   });
+  // Paises
+  $("#nacionalidad").on("change", function () {
+    if (this.value == "salvadoreno") {
+      $("#pais")
+        .append(`<option value="sv">El Salvador</option> `)
+        .prop("disabled", true);
+    } else {
+      $("#pais").prop("disabled", false);
+      $("#pais").select2({
+        width: "100%",
+        ajax: {
+          url: "https://api.salud.gob.sv/pais",
+          dataType: "json",
+          delay: 250,
+          data(params) {
+            return {
+              nombre: params.term,
+            };
+          },
+          processResults: function (data) {
+            return {
+              results: $.map(data, function (obj, index) {
+                return { id: obj.id, text: obj.nombre };
+              }),
+            };
+          },
+          cache: true,
+        },
+      });
+    }
+  });
+
   // FIN SECCION 1 =====================================================================
   // SECCION 2 =========================================================================
   // Sabe leer escribir
@@ -82,6 +149,15 @@ $(document).ready(function () {
     }
   });
 
+  $(".beneficios").on("change", function () {
+    if (this.value == "Si") {
+      $("#cursopositivo").css("display", "block");
+    }
+    if (this.value == "No") {
+      $("#cursonegativo").css("display", "block");
+    }
+  });
+
   // FIN SECCION 2 =====================================================================
 
   // SECCION 3 =====================================================================
@@ -104,6 +180,18 @@ $(document).ready(function () {
     }
   });
 
+  $("#tipoempleo").on("change", function () {
+    if (
+      this.value == "tmpcompleto" ||
+      this.value == "tmpparcial" ||
+      this.value == "temporal"
+    ) {
+      $("#sectordetraajo").css("display", "block");
+    } else {
+      $("#sectordetraajo").css("display", "none");
+    }
+  });
+
   // FIN SECCION 3 =====================================================================
   // SECCION 4 =====================================================================
   // Si recibe ingresos
@@ -115,4 +203,152 @@ $(document).ready(function () {
     }
   });
   // FIN SECCION 4 =====================================================================
+  //SELECTS
+  $("#depa_nac").select2({
+    width: "100%",
+    ajax: {
+      url: "https://api.salud.gob.sv/departamentos",
+      dataType: "json",
+      delay: 250,
+      data(params) {
+        return {
+          idPais: 68,
+          nombre: params.term,
+        };
+      },
+      processResults: function (data) {
+        return {
+          results: $.map(data, function (obj, index) {
+            return { id: obj.id, text: obj.nombre };
+          }),
+        };
+      },
+      cache: true,
+    },
+  });
+
+  $("#depa_nac").on("select2:select", function (e) {
+    var idDepartamento = e.params.data.id;
+    $("#municipioNac").select2({
+      width: "100%",
+      ajax: {
+        url: "https://api.salud.gob.sv/municipios",
+        dataType: "json",
+        delay: 250,
+        data(params) {
+          return {
+            idDepartamento,
+            nombre: params.term,
+          };
+        },
+        processResults: function (data) {
+          return {
+            results: $.map(data, function (obj, index) {
+              return { id: obj.id, text: obj.nombre };
+            }),
+          };
+        },
+        cache: true,
+      },
+    });
+  });
+
+  $("#depdomicilio").select2({
+    width: "100%",
+    ajax: {
+      url: "https://api.salud.gob.sv/departamentos",
+      dataType: "json",
+      delay: 250,
+      data(params) {
+        return {
+          idPais: 68,
+          nombre: params.term,
+        };
+      },
+      processResults: function (data) {
+        return {
+          results: $.map(data, function (obj, index) {
+            return { id: obj.id, text: obj.nombre };
+          }),
+        };
+      },
+      cache: true,
+    },
+  });
+
+  $("#depdomicilio").on("select2:select", function (e) {
+    var idDepartamento = e.params.data.id;
+    $("#municipioDomicilio").select2({
+      width: "100%",
+      ajax: {
+        url: "https://api.salud.gob.sv/municipios",
+        dataType: "json",
+        delay: 250,
+        data(params) {
+          return {
+            idDepartamento,
+            nombre: params.term,
+          };
+        },
+        processResults: function (data) {
+          return {
+            results: $.map(data, function (obj, index) {
+              return { id: obj.id, text: obj.nombre };
+            }),
+          };
+        },
+        cache: true,
+      },
+    });
+  });
+
+  $("#departcontact").select2({
+    width: "100%",
+    ajax: {
+      url: "https://api.salud.gob.sv/departamentos",
+      dataType: "json",
+      delay: 250,
+      data(params) {
+        return {
+          idPais: 68,
+          nombre: params.term,
+        };
+      },
+      processResults: function (data) {
+        return {
+          results: $.map(data, function (obj, index) {
+            return { id: obj.id, text: obj.nombre };
+          }),
+        };
+      },
+      cache: true,
+    },
+  });
+
+  $("#departcontact").on("select2:select", function (e) {
+    var idDepartamento = e.params.data.id;
+    $("#municipiocontacto").select2({
+      width: "100%",
+      ajax: {
+        url: "https://api.salud.gob.sv/municipios",
+        dataType: "json",
+        delay: 250,
+        data(params) {
+          return {
+            idDepartamento,
+            nombre: params.term,
+          };
+        },
+        processResults: function (data) {
+          return {
+            results: $.map(data, function (obj, index) {
+              return { id: obj.id, text: obj.nombre };
+            }),
+          };
+        },
+        cache: true,
+      },
+    });
+  });
+  //FIN SELECTS
 });
