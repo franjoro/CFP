fbdf292715b6c7eaf3fbc6472de248b1ecf54591
OsmaroB <36721551+OsmaroB@users.noreply.github.com>
2021-10-26 12:02:19 -0600

    Enroll participants

--- public/js/habil/detalle/tables.js
+++ public/js/habil/detalle/tables.js
@@ -10,6 +10,22 @@ const applicationTable = () => {
         
       },
       columns: [
+        {
+          render(data,type,row){
+            html = `
+           
+            <span>
+              <div class="form-check">
+                <input class="form-check-input ck" type="checkbox" value="${row.idSolicitud}" id="ck${row.idSolicitud}">
+                <label class="form-check-label" for="ck${row.idSolicitud}">
+                 <p>N°<span class="negrita"></span></p>       
+                </label>
+              </div>
+            </span>`;
+            return(html);
+
+          }
+        },
         {render(data,type,row){
           html = `${row.fecha_inscripcion }`;
           return(html);
@@ -17,9 +33,13 @@ const applicationTable = () => {
         { render(data, type,row){
           let html;
           if(row){
-            html = `<div class="" id="row${row.idSolicitud}">${row.dui}</div>`
+            html = `<div class="" id="row${row.idSolicitud}">${row.dui}</div>
+            <span>
+              <button class = 'btn btn-danger' id='d${row.idSolicitud}'>
+              </button>
+            </span>`
           }else{
-            html = `<div class="bg-danger">${row.dui}</div>`
+            html = `<div class="bg-danger">${row.dui}</div><span><button class = 'btn btn-danger' id='${row.idSolicitud}'></button></span>`
           }
             return(html)
           }
@@ -101,7 +121,9 @@ const changeColor= async () =>{
     $.each(data.data, function(i, item){
       //Cambiamos el color del id
       try {
-        $(`#row${item.idSolicitud}`).addClass('bg-info text-white');
+        // $(`#row${item.idSolicitud}`).addClass('bg-info text-white');
+        $(`#row${item.idSolicitud}`).append(`<span><button class= 'btn btn-info'></button><span>`);
+        $(`#d${item.idSolicitud}`).remove();
 
       } catch (error) {
         
@@ -126,7 +148,7 @@ const changeColorWait = async () =>{
     $.each(data.data, function(i, item){
       //Cambiamos el color del id
       try {
-        $(`#row${item.idSolicitud}`).addClass('bg-warning text-black');
+        $(`#row${item.idSolicitud}`).append(`<span><button class= 'btn btn-warning'></button><span>`);
       } catch (error) {
         
       }
@@ -134,9 +156,96 @@ const changeColorWait = async () =>{
   }
 };
 
+const countParticipants = async () =>{
+  await $(".negrita").each(function(index,element){
+    $(element).text((index+1));
+  });
+};
+
+const selectParticipants = async () =>{
+  const arr = [];
+  await $(".ck").each((index,element)=>{
+    let idRequest = $(element).val();
+    if($(element).is(":checked")){
+      arr.push(idRequest);
+    }
+  });
+  localStorage.setItem('participants',JSON.stringify(arr));
+  
+};
+
+const enrollParticipants = () =>{
+  $(JSON.parse(localStorage.getItem('participants'))).each((index, element) =>{
+    try {
+      const data =  $.ajax({
+        url: "/admin/habil/matricular",
+        type: "PUT",
+        data: {
+          cmbCurso: $("#txtIdSolicitudC").val(),
+          txtIdSolicitud: $("#cmbCursoC").val()
+        },
+      });
+      if (data.status) {
+        swal.close();
+        Swal.fire(
+          `${(index+1)} participante matriculado correctamente`,
+          "success"
+        );
+        $("#modal_matricular_c").modal("hide");
+      }
+    } catch (error) {
+      swal.close();
+      console.log(error);
+      errorMessage();
+    }
+  });
+};
+
+const classClick = () =>{
+  $(document).on('click','.ck',() =>{
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    if( $('.ck').is(':checked') ) {
+      blockChecked();
+    }else{
+      $(".btnBlock").remove();
+    }
+  });
+};
+
+const blockChecked = () =>{
+  setTimeout(function(){
+    $(".btnBlock").remove();
+    // $(".blockChecked").css({'background-color': '#F4F4F4'})
+    // $('#blockChecked').tooltip(options)
+    // $('#blockChecked').tooltip('enable');
+    $("#blockChecked").append(`
+    <button 
+      class="btn btn-success btnBlock btn-sm"
+      data-toggle="modal" data-target="#modal_matricular_c"
+      onclick="selectParticipants()"
+    >
+      <i class="fas fa-exchange-alt"></i>
+    </button>
+    <button 
+      class="btn btn-danger btnBlock btn-sm"
+      data-toggle="tooltip" data-placement="top" title="Eliminar estudiantes seleccionados"
+    >
+      <i class="fas fa-trash-alt"></i>
+    </button>
+  `);
+  },100)
+  
+};
+
 $(document).ready(function () {
+  $("#btnEnrollParticipants").click(() =>{
+    enrollParticipants();
+  });
   applicationTable();
   setTimeout(changeColorWait, 100);
   setTimeout(changeColor, 100);
+  setTimeout(countParticipants, 100);
+  classClick();
 });
 
