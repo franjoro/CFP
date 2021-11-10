00133d7e02937ec0902ae168625cd1bfdbd0d583
OsmaroB <36721551+OsmaroB@users.noreply.github.com>
2021-11-10 11:08:59 -0600

    Add count

--- controllers/cursos.controller.js
+++ controllers/cursos.controller.js
@@ -35,11 +35,8 @@ cursos.cursos = async (req, res) => {
 
     queries.push(
       pool.query(
-        `SELECT CONCAT(Nombre,' - ',Horario) AS Nombre , Codigo_curso, (SELECT COUNT(*) FROM tb_habil_solicitudes 
-        WHERE Codigo_curso = tb_cursos.Codigo_curso ) AS cantidadAlumnos , 
-        (SELECT COUNT(*) FROM tb_habil_solicitudes as sol1 INNER JOIN tb_habil_documentos as doc1 ON sol1.id = doc1.id_solicitud AND sol1.Codigo_curso = tb_cursos.Codigo_curso) as cantIncom,
-      
-        (SELECT COUNT(*) FROM union_curso_empresa 
+        `SELECT CONCAT(Nombre,' - ',Horario) AS Nombre , Codigo_curso, (SELECT COUNT(*) FROM union_matricula 
+        WHERE id_curso = tb_cursos.Codigo_curso ) AS cantidadAlumnos , (SELECT COUNT(*) FROM union_curso_empresa 
         WHERE id_curso = tb_cursos.Codigo_curso ) AS cantidadEmpresas , Estado , tb_cursos.Date_inicio as fecha_inicio, 
         tb_cursos.Date_fin as fecha_fin FROM tb_cursos WHERE (Estado = 5 || Estado = 15) AND id_programa=?;`,
         [programa]
