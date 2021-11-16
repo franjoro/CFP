6f68f838cdc7a0dfef745ef950aaa31e2be4f369
OsmaroB <36721551+OsmaroB@users.noreply.github.com>
2021-11-16 14:14:48 -0600

    .

--- public/js/habil/detalle/tables.js
+++ public/js/habil/detalle/tables.js
@@ -88,7 +88,7 @@ const applicationTable = () => {
           render(data,type,row){
             console.log(row.nombre)
             const html = `
-              <p> ${firtWordUppercase(row.nombre)}</p>
+              <p> ${(row.nombre)}</p>
               
             `
             return(html);
@@ -97,7 +97,7 @@ const applicationTable = () => {
         {
           render(data,type,row){
             const html = `
-              <p> ${firtWordUppercase(row.apellidos)}</p>
+              <p> ${(row.apellidos)}</p>
               
             `
             return(html);
