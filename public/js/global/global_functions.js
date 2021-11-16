c3c854fc96f66c4069febdb56147d28c46fa2e7f
OsmaroB <36721551+OsmaroB@users.noreply.github.com>
2021-11-16 14:14:01 -0600

    .

--- public/js/global/global_functions.js
+++ public/js/global/global_functions.js
@@ -101,13 +101,18 @@ const toastSucces = (msg) =>{
 
 
 
-// const firtWordUppercase = (text)=>{
-//   return text
-//   .toLowerCase()
-//     .split(' ')
-//     .map(function(Word) {
-//         return Word[0].toUpperCase() + Word.substr(1);
-//     })
-//     .join(' ');
-// };
+const firtWordUppercase = (text)=>{
+  return text
+  .toLowerCase()
+    .split(' ')
+    .map(function(Word) {
+        if(Word[0] !=""){
+          return Word[0].toUpperCase() + Word.substr(1);
+        }else{
+          return text
+        }
+        
+    })
+    .join(' ');
+};
  
