e0bcb885b6bdfe1582b9fbec406bb1aa54f62fe0
OsmaroB <36721551+OsmaroB@users.noreply.github.com>
2021-11-23 07:54:44 -0600

    Add function button sendOtherFiles for habil proes

--- public/js/habil/documentacion/subidaDatos.js
+++ public/js/habil/documentacion/subidaDatos.js
@@ -4,7 +4,6 @@
   @param: Null
   @*/
 const SendFiles = async (typeDocument) => {
-  console.log(typeDocument);
   
   //Recolectamos los valores de los input
     let idSolicitud = $("#id_solicitud").val();
@@ -88,3 +87,58 @@ const SendFiles = async (typeDocument) => {
     }
   };
 
+const sendOtherFile = async()=>{
+  //Recolectamos los valores de los input
+  let idSolicitud = $("#idSolicitud").val();
+  let typeDocument = $("#documentType").val();
+
+  //Objeto de js utilizado para mandar ficheros
+  const fd = new FormData(); // Objeto de javascript utilizado para mandar documentos
+  //Recolectamos la longitud de archivos almacenados tanto en plantilla, recibo y cancelacion
+  let cantidadDocumentos;
+  
+  cantidadDocumentos = $(`#documentFile`)[0].files.length;
+  //Hacemos un for a pesar de que sea solo un file tiene diferentes archivos por eso mandamos un for
+  for (let i = 0; i < cantidadDocumentos; i++) {
+    //append para anidar y ponemos el name y id del file
+    fd.append(`fileDocumentos${i}`, $(`#documentFile`)[0].files[i]);  
+  }
+  
+  //Usamos append para anidar la cantidad de planilla al FormData fd para manejar documentos
+  fd.append("cantidadDocumentos", cantidadDocumentos);
+  //AGREGAMOS LAS VARIABLES A ENVIAR AL SERVER
+  fd.append("idSolicitud", idSolicitud);
+
+  //Ejecutamos un try e intentamos mandar los archivos a la nube
+  try {
+    //ventana emergente de carga
+    archivosLoader();
+    //const respuesta para recolectar lo traido del .ajax
+    const respuesta = await $.ajax({
+      url: `/habil/EnviarFiles/${typeDocument}`,
+      type: "POST",
+      data: fd,
+      processData: false,
+      contentType: false,
+    });
+    //validamos el estado de la const respuesta
+    if(respuesta.status){
+      swal.close();
+      Swal.fire({
+        icon: "success",
+        title: "Solicitud enviada correctamente",
+        showConfirmButton: false,
+      });
+      window.location.reload();
+    }else{
+      swal.close();
+      error(`No se pudo realizar la operación, verifica la información o comuniquese con el encargado del programa o soporte ${respuesta.error}`);
+    }
+  } catch (e) {
+    console.log(e);
+    error(
+      "No se pudo realizar la operación, verifica la información o comuniquese con el encargado del programa o soporte  <b> errorcode: </b>  " +
+        e.responseJSON.error
+    );
+  }
+};
